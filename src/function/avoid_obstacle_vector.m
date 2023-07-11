6df041e3c8aeb85a4fc089ac49ff875435f3d928
Sing Song <Sangbuster911@yahoo.com>
2023-07-11 09:15:57 -0400

    clean dir

--- src/function/avoid_obstacle_vector.m
+++
@@ -1,5 +0,0 @@
-function V = avoid_obstacle_vector(x_i, y_i, x_o, y_o, V_move_to_goal)
-robot_to_obstacle = [ (y_o - y_i); -1 * (x_o - x_i)];
-% sign_pm = sign(dot(V_move_to_goal, robot_to_obstacle, 1));
-V = (robot_to_obstacle / norm(robot_to_obstacle));
-end
--- src/function/control_parameter_f1.m
+++
@@ -1,7 +0,0 @@
-function f1 = control_parameter_f1(d_0, a_m, b_m)
-if d_0 > b_m
-    f1 = a_m;
-else
-    f1 = a_m * (d_0 / b_m);
-end
-end
